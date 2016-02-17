76d2361fb6b408cc64417a4e1ce8896a17b74174
Manuel Salinas <salinas.manuel_1980@yahoo.com>
2016-02-17 15:27:16 -0600

    removed title bar by editing androidmanifest.xml and modelchooser.java

--- app/src/main/java/com/paar/ch8/ModelChooser.java
+++ app/src/main/java/com/paar/ch8/ModelChooser.java
@@ -16,6 +16,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -25,6 +26,7 @@ public class ModelChooser extends ListActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		AssetManager am = getAssets();
 		Vector<Item> models = new Vector<Item>();
 		Item item = new Item();
